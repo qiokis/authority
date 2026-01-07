1aac5d74f3b3256d6b0eb43e0b619a19fd97766a
Anton Pechkin <a.d.pechkin@yandex.ru>
2026-01-07 14:23:27 +0300

    Feature. Add alembic configuration with default conf

--- alembic/env.py
+++ alembic/env.py
@@ -15,6 +15,9 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 from authority.models.base import BaseModel
+from authority import Config
+config = Config()
+
 target_metadata = BaseModel.metadata
 
 
@@ -30,9 +33,13 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        url=(
+            "postgres://"
+                f"{config.db.user}:{config.db.passw}@"
+                f"{config.db.host}:{config.db.port}"
+                f"/{config.db.name}"
+        ),
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -50,7 +57,14 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        {
+            "sqlalchemy.url":(
+                "postgresql://"
+                    f"{config.db.user}:{config.db.passw}@"
+                    f"{config.db.host}:{config.db.port}"
+                    f"/{config.db.name}"
+            )
+        },
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
